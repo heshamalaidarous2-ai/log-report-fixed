50c9f6462a90d5a8025d03f7beae8b1e374c132f
heshamalaidarous2-ai <heshamalaidarous2@gmail.com>
2026-07-04 01:48:38 -0500

    Create test.sh

---
+++ solution/tests/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -uo pipefail
+
+pytest /tests/test_outputs.py -rA --ctrf /app/ctrf.json
+status=$?
+
+if [ "$status" -eq 0 ]; then
+  echo 1 > /app/reward.txt
+else
+  echo 0 > /app/reward.txt
+fi
+
+exit 0
